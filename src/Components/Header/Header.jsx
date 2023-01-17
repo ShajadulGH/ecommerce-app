c4f2f4791835ef60a1eeab6eadefa053b7dfa243
Shajadul <shajadulrahe@gmail.com>
2023-01-17 20:24:55 +0600

    feat: Header Section

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,7 +1,89 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./Header.module.scss";
-const Header = () => {
-  return <div>Header</div>;
-};
+import { Link, NavLink } from "react-router-dom";
+import { AiOutlineShoppingCart } from "react-icons/ai";
+import { HiOutlineMenuAlt3 } from "react-icons/hi";
+const logo = (
+  <div className={styles.logo}>
+    <Link to="/">
+      <h2>
+        <span>Shop</span>.in
+      </h2>
+    </Link>
+  </div>
+);
+const cart = (
+  <span className={styles.cart}>
+    <Link to="/cart">
+      Cart
+      <AiOutlineShoppingCart size={20} />
+      <p>0</p>
+    </Link>
+  </span>
+);
+const activeLink = ({ isActive }) => (isActive ? `${styles.active}` : "");
 
+const Header = () => {
+  const [showMenu, setShowMenu] = useState(false);
+  // const [toggleMenu, setToggleMenu] = useState(false);
+  const toggleMenu = () => {
+    setShowMenu(!showMenu);
+  };
+  const hideMenu = () => {
+    setShowMenu(false);
+  };
+  return (
+    <header className={styles.fixed}>
+      <div className={styles.header}>
+        {logo}
+        <nav
+          className={
+            showMenu ? `${styles["show-nav"]}` : `${styles["hide-nav"]}`
+          }
+        >
+          <div
+            className={
+              showMenu
+                ? `${styles["nav-wrapper"]} ${styles["show-nav-wrapper"]}`
+                : `${styles["nav-wrapper"]}`
+            }
+            onClick={hideMenu}
+          ></div>
+          <ul>
+            <li className={styles["logo-mobile"]}>{logo}</li>
+            <li>
+              <NavLink to="/" className={activeLink}>
+                Home
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/contact" className={activeLink}>
+                Contact Us
+              </NavLink>
+            </li>
+          </ul>
+          <div className={styles["header-right"]} onClick={hideMenu}>
+            <span className={styles.links}>
+              <NavLink to="/login" className={activeLink}>
+                Login
+              </NavLink>
+              <NavLink to="/register" className={activeLink}>
+                Register
+              </NavLink>
+              <NavLink to="/my-orders" className={activeLink}>
+                My Orders
+              </NavLink>
+            </span>
+            {cart}
+          </div>
+        </nav>
+
+        <div className={styles["menu-icon"]}>
+          {/* {cart} */}
+          <HiOutlineMenuAlt3 size={28} onClick={toggleMenu} />
+        </div>
+      </div>
+    </header>
+  );
+};
 export default Header;
