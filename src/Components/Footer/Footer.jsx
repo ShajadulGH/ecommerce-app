18449e573ebe7a9248510395cfd59e6d70d9bd48
Shajadul <shajadulrahe@gmail.com>
2023-01-17 20:57:40 +0600

    feat: footer section

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,7 +1,10 @@
 import React from "react";
 import styles from "./Footer.module.scss";
+const date = new Date();
+const currentYear = date.getFullYear();
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className={styles.footer}>&copy; {currentYear} All Right Reserved</div>
+  );
 };
-
 export default Footer;
