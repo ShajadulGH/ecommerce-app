ac4b4950b66bc124ed17d32938d0558e40eb4420
Shajadul <shajadulrahe@gmail.com>
2023-01-18 10:25:35 +0600

    feat: set route for login, register, reset

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 // Pages
 import { Home, Contact } from "./Pages/ExportPages";
+import { Login, Register, Reset } from "./Pages/ExportPages";
 // Componets
 import { Header, Footer } from "./Components/ExportComponents";
 function App() {
@@ -9,8 +10,11 @@ function App() {
       <BrowserRouter>
         <Header />
         <Routes>
-          <Route path="/home" element={<Home />} />
+          <Route path="/" element={<Home />} />
           <Route path="/contact" element={<Contact />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/reset" element={<Reset />} />
         </Routes>
         <Footer />
       </BrowserRouter>
--- src/Pages/ExportPages.js
+++ src/Pages/ExportPages.js
@@ -1,2 +1,5 @@
 export { default as Home } from "./Home/Home.jsx";
 export { default as Contact } from "./Contact/Contact.jsx";
+export { default as Login } from "./Authentication/Login";
+export { default as Register } from "./Authentication/Register";
+export { default as Reset } from "./Authentication/Reset";
