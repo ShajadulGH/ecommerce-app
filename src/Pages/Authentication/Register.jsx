230ee5dcc23e58a41c1eaf947a3fabde73bf8d07
Shajadul <shajadulrahe@gmail.com>
2023-01-30 11:34:08 +0600

    feat: configure registration

--- src/Pages/Authentication/Register.jsx
+++ src/Pages/Authentication/Register.jsx
@@ -2,15 +2,38 @@ import React from "react";
 import { Link } from "react-router-dom";
 import styles from "./Auth.module.scss";
 import LoginImage from "../../assets/Images/register.jpg";
+import { useState } from "react";
 const Register = () => {
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+  const [confirmPassword, setConfirmPassword] = useState();
+  const submitHandler = (e) => {
+    e.preventDefault();
+    console.log(email, password, confirmPassword);
+  };
   return (
     <section className={`container ${styles.auth}`}>
       <div className={styles.form}>
         <h2>Register</h2>
-        <form>
-          <input type="text" placeholder="Email" required />
-          <input type="password" placeholder="Password" required />
-          <input type="password" placeholder="Confirm Password" required />
+        <form onSubmit={submitHandler}>
+          <input
+            onChange={(e) => setEmail(e.target.value)}
+            type="text"
+            placeholder="Email"
+            required
+          />
+          <input
+            onChange={(e) => setPassword(e.target.value)}
+            type="password"
+            placeholder="Password"
+            required
+          />
+          <input
+            onChange={(e) => setConfirmPassword(e.target.value)}
+            type="password"
+            placeholder="Confirm Password"
+            required
+          />
           <button type="submit" className="--btn --btn-amazon --btn-block">
             Register
           </button>
