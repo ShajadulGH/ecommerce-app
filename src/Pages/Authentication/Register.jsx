f73e5570f2b658b61112652a9a4c090f8d7b7c65
Shajadul <shajadulrahe@gmail.com>
2023-02-03 19:02:18 +0600

    feat: error notificaton on password miss match

--- src/Pages/Authentication/Register.jsx
+++ src/Pages/Authentication/Register.jsx
@@ -3,50 +3,58 @@ import { Link } from "react-router-dom";
 import styles from "./Auth.module.scss";
 import LoginImage from "../../assets/Images/register.jpg";
 import { useState } from "react";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 const Register = () => {
   const [email, setEmail] = useState();
   const [password, setPassword] = useState();
   const [confirmPassword, setConfirmPassword] = useState();
   const submitHandler = (e) => {
     e.preventDefault();
+    if (password !== confirmPassword) {
+      toast.error("Password Miss Match!!!");
+    }
     console.log(email, password, confirmPassword);
   };
   return (
-    <section className={`container ${styles.auth}`}>
-      <div className={styles.form}>
-        <h2>Register</h2>
-        <form onSubmit={submitHandler}>
-          <input
-            onChange={(e) => setEmail(e.target.value)}
-            type="text"
-            placeholder="Email"
-            required
-          />
-          <input
-            onChange={(e) => setPassword(e.target.value)}
-            type="password"
-            placeholder="Password"
-            required
-          />
-          <input
-            onChange={(e) => setConfirmPassword(e.target.value)}
-            type="password"
-            placeholder="Confirm Password"
-            required
-          />
-          <button type="submit" className="--btn --btn-amazon --btn-block">
-            Register
-          </button>
-        </form>
-        <span className={styles.register}>
-          <p>Already have an account?</p>
-          <Link to="/login">Login</Link>
-        </span>
-      </div>
-      <div className={styles.img}>
-        <img src={LoginImage} alt="Login" width={400} />
-      </div>
-    </section>
+    <>
+      <ToastContainer />
+      <section className={`container ${styles.auth}`}>
+        <div className={styles.form}>
+          <h2>Register</h2>
+          <form onSubmit={submitHandler}>
+            <input
+              onChange={(e) => setEmail(e.target.value)}
+              type="text"
+              placeholder="Email"
+              required
+            />
+            <input
+              onChange={(e) => setPassword(e.target.value)}
+              type="password"
+              placeholder="Password"
+              required
+            />
+            <input
+              onChange={(e) => setConfirmPassword(e.target.value)}
+              type="password"
+              placeholder="Confirm Password"
+              required
+            />
+            <button type="submit" className="--btn --btn-amazon --btn-block">
+              Register
+            </button>
+          </form>
+          <span className={styles.register}>
+            <p>Already have an account?</p>
+            <Link to="/login">Login</Link>
+          </span>
+        </div>
+        <div className={styles.img}>
+          <img src={LoginImage} alt="Login" width={400} />
+        </div>
+      </section>
+    </>
   );
 };
 
