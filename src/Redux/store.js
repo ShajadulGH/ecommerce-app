b499f6bcd9593903d3961bf9369921439c34ef03
Shajadul <shajadulrahe@gmail.com>
2023-03-25 14:50:10 +0600

    feat: setup redux

---
+++ src/Redux/store.js
@@ -0,0 +1,8 @@
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
+
+const rootReducer = combineReducers();
+
+const store = configureStore({
+  reducer: rootReducer,
+});
+export default store;
