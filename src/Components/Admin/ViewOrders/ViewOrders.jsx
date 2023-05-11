782cf5deb17d86a799f969ef921b48745e15fc1f
Shajadul <shajadulrahe@gmail.com>
2023-05-11 16:24:53 +0600

    feat: Add admin nav, content and nested routes

---
+++ src/Components/Admin/ViewOrders/ViewOrders.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ViewOrders = () => {
+  return <div>ViewOrders</div>;
+};
+
+export default ViewOrders;
--- src/Pages/Admin/Admin.jsx
+++ src/Pages/Admin/Admin.jsx
@@ -1,7 +1,28 @@
 import React from "react";
 import styles from "./Admin.module.scss";
+import Navbar from "../../Components/Admin/Navbar/Navbar";
+import Home from "../../Components/Admin/Home/Home";
+import AddProduct from "../../Components/Admin/AddProduct/AddProduct";
+import ViewProducts from "../../Components/Admin/ViewProducts/ViewProducts";
+import ViewOrders from "../../Components/Admin/ViewOrders/ViewOrders";
+import { Route, Routes } from "react-router-dom";
+
 const Admin = () => {
-  return <div>Admin</div>;
+  return (
+    <div className={styles.admin}>
+      <div className={styles.navbar}>
+        <Navbar />
+      </div>
+      <div className={styles.content}>
+        <Routes>
+          <Route path="home" element={<Home />} />
+          <Route path="add-product" element={<AddProduct />} />
+          <Route path="view-products" element={<ViewProducts />} />
+          <Route path="orders" element={<ViewOrders />} />
+        </Routes>
+      </div>
+    </div>
+  );
 };
 
 export default Admin;
