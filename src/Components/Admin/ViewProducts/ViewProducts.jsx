0a165ff51ff938d9dabecb6a2f5ca729b07cc58a
Shajadul <shajadulrahe@gmail.com>
2023-06-01 20:20:16 +0600

    feat - display products

--- src/Components/Admin/ViewProducts/ViewProducts.jsx
+++ src/Components/Admin/ViewProducts/ViewProducts.jsx
@@ -9,6 +9,7 @@ import {
 } from "firebase/firestore";
 import { db } from "../../../Firebase/config";
 import { toast } from "react-toastify";
+import { FaEdit, FaTrashAlt } from "react-icons/fa";
 const ViewProducts = () => {
   const [allProducts, setAllProducts] = useState([]);
   useEffect(() => {
@@ -30,7 +31,53 @@ const ViewProducts = () => {
       toast.error(error.message);
     }
   };
-  return <div>ViewProducts</div>;
+  return (
+    <>
+      <div className={styles.table}>
+        <h2>All Products</h2>
+        {allProducts.length === 0 ? (
+          <p>Product list is empty!</p>
+        ) : (
+          <table>
+            <thead>
+              <tr>
+                <th>s/n</th>
+                <th>Image</th>
+                <th>Name</th>
+                <th>Category</th>
+                <th>Price</th>
+                <th>Actions</th>
+              </tr>
+            </thead>
+            <tbody>
+              {allProducts.map((product, index) => {
+                const { id, name, price, imageURL, category } = product;
+                return (
+                  <tr key={id}>
+                    <td>{index + 1}</td>
+                    <td>
+                      <img
+                        src={imageURL}
+                        alt={name}
+                        style={{ width: "100px" }}
+                      />
+                    </td>
+                    <td>{name}</td>
+                    <td>{category}</td>
+                    <td>{`৳${price}`}</td>
+                    <td className={styles.icons}>
+                      <FaEdit size={20} color="green" />
+                      &nbsp;
+                      <FaTrashAlt size={18} color="red" />
+                    </td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        )}
+      </div>
+    </>
+  );
 };
-
 export default ViewProducts;
