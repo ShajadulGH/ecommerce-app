f0e87010ae2b27022a421f51a130bf3da0a0bc4b
Shajadul <shajadulrahe@gmail.com>
2023-06-08 10:17:21 +0600

    fix - getDefaultMiddleware

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -8,5 +8,9 @@ const rootReducer = combineReducers({
 
 const store = configureStore({
   reducer: rootReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
 });
 export default store;
