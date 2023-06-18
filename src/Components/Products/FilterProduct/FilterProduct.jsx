89efec8e3e5c36082a00ac35f99a52cb9676508b
Shajadul <shajadulrahe@gmail.com>
2023-06-18 10:59:23 +0600

    feat - product filter section

--- src/Components/Products/FilterProduct/FilterProduct.jsx
+++ src/Components/Products/FilterProduct/FilterProduct.jsx
@@ -1,7 +1,28 @@
 import React from "react";
-
+import styles from "./Filter.module.scss";
 const Filter = () => {
-  return <div>Filter</div>;
+  return (
+    <div className={styles.filter}>
+      <h4>Categories</h4>
+      <div className={styles.category}>
+        <button type="button">&#8250; All </button>
+      </div>
+      <h4>Brand</h4>
+      <div className={styles.brand}>
+        <select>
+          <option value="ALL">All</option>
+        </select>
+
+        <h4>Price</h4>
+        <p>1000</p>
+        <div className={styles.price}>
+          <input type="range" min="100" max="1000" />
+        </div>
+        <br />
+        <button className="--btn --btn-amazon">Clear Filter</button>
+      </div>
+    </div>
+  );
 };
 
 export default Filter;
