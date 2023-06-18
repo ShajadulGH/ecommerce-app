1022e7797f83820be5381b3ff2541a0329844c7b
Shajadul <shajadulrahe@gmail.com>
2023-06-18 10:33:54 +0600

    feat - scroll to products when back on home page from product details

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -2,7 +2,22 @@ import React from "react";
 import styles from "./Home.module.scss";
 import Products from "../../Components/Products/Products";
 import Slider from "../../Components/Slider/Slider";
+import { useEffect } from "react";
 const Home = () => {
+  const url = window.location.href;
+  const scrollToProducts = () => {
+    if (url.includes("#products")) {
+      window.scrollTo({
+        top: 750,
+        behavior: "smooth",
+      });
+    }
+  };
+  useEffect(() => {
+    setTimeout(() => {
+      scrollToProducts();
+    }, 100);
+  }, []);
   return (
     <div>
       <Slider />
