b26ca2c9ff789abc02ed66482a0a5995c721d821
Shajadul <shajadulrahe@gmail.com>
2023-07-19 11:58:19 +0600

    feat - show hide filter

--- src/Components/Products/Products.jsx
+++ src/Components/Products/Products.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./Products.module.scss";
 import FilterProduct from "./FilterProduct/FilterProduct";
 import ProductList from "./ProductList/ProductLIst";
@@ -9,9 +9,12 @@ import {
   GET_PRICE_RANGE,
   selectProducts,
 } from "../../Redux/Features/prouctsSlice";
+import { FaCogs } from "react-icons/fa";
 
 const Products = () => {
   const { allProducts, isLoading } = FetchData("products");
+  const [showFilter, setShowFilter] = useState(false);
+
   console.log(allProducts);
   const products = useSelector(selectProducts);
   console.log(products);
@@ -31,14 +34,27 @@ const Products = () => {
       })
     );
   }, [allProducts, dispatch]);
+  const toggleFilter = () => {
+    setShowFilter(!showFilter);
+  };
   return (
     <section>
       <div className={`container ${styles.product}`}>
-        <aside className={styles.filter}>
-          <FilterProduct />
+        <aside
+          className={
+            showFilter ? `${styles.filter} ${styles.show}` : `${styles.filter}`
+          }
+        >
+          {isLoading ? null : <FilterProduct />}
         </aside>
         <div className={styles.content}>
           <ProductList products={allProducts} />
+          <div className={styles.icon} onClick={toggleFilter}>
+            <FaCogs size={20} color="orangered" />
+            <p>
+              <b>{showFilter ? "Hide Filter" : "Show Filter"}</b>
+            </p>
+          </div>
         </div>
       </div>
     </section>
